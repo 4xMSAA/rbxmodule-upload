d36a471d29fd3c39180dfae3fe45a6db237e2f14
Mikyu Amber Lynne <27443298+4xMSAA@users.noreply.github.com>
2020-03-11 04:54:13 +0200

    XML Roblox Instance tree generation

--- upload.py
+++ upload.py
@@ -1,12 +1,19 @@
+# a poorly made Python script meant for building a MainModule XML with Rojo-like structure
+
 import argparse
 import json
-import io
 import os
+import re
 
 import lxml.etree as ET
 import pip._vendor.requests as requests
 
+from pathlib import Path
+
 parser = argparse.ArgumentParser()
+
+parser.add_argument("source",
+                    help="the source folder to be written as XML output")
 parser.add_argument(
     "-a",
     "--auth",
@@ -20,9 +27,7 @@ parser.add_argument(
     default="https://data.roblox.com/Data/Upload.ashx?json=1&assetid=10",
     help="request destination - defaults to /Data/Upload.ashx?json=1&assetid=10"
 )
-parser.add_argument("source",
-                    help="the source folder to be written as XML output")
-parser.add_argument("-o",
+parser.add_argument("-O",
                     "--output",
                     dest="output",
                     help="output XML file destination")
@@ -31,11 +36,12 @@ args = parser.parse_args()
 
 
 def new_plain_rbxmx():
-    root = ET.Element("roblox",
-                      nsmap={
-                          "xmime": "http://www.w3.org/2005/05/xmlmime",
-                      },
-                      )
+    root = ET.Element(
+        "roblox",
+        nsmap={
+            "xmime": "http://www.w3.org/2005/05/xmlmime",
+        },
+    )
     root.set("version", "4")
 
     external = ET.Element("External", {})
@@ -56,6 +62,19 @@ def new_instance(className, parent):
     parent.append(inst)
 
     return inst
+
+
+def set_name(element, obj_name):
+    Properties = ET.Element("Properties")
+
+    name = ET.Element("string")
+    name.text = obj_name
+    name.set("name", "Name")
+
+    Properties.append(name)
+    element.append(Properties)
+
+    return Properties
 
 
 def give_script_properties(element, obj_name, src):
@@ -87,24 +106,76 @@ def give_script_properties(element, obj_name, src):
     return Properties
 
 
-def uploadToRoblox(moduleXML):
+def upload_to_roblox(moduleXML):
     requests.post(args.uploadUri,
                   data=moduleXML,
                   headers={'Content-Type': 'application/xml'},
                   cookies={".ROBLOSECURITY": args.cookie})
 
 
-def writeOutput(dest):
+def write_output(dest):
     tree = ET.ElementTree(root)
     tree.write(dest, encoding="UTF-8")
 
 
-def fsToTree(folder):
+def get_instance_type_from_extension(filename):
+    if (filename.endswith(".client.lua")):
+        return "LocalScript"
+    elif (filename.endswith(".server.lua")):
+        return "Script"
+    elif (filename.endswith(".lua")):
+        return "ModuleScript"
+
+
+# really messy, could be cleaned up
+def fs_to_tree(folder, root_element):
+    element_roots = {str(folder): root_element}
+    for root, dirs, files in os.walk(folder, topdown=True):
+        print(root, dirs, files)
+        dirname = os.path.basename(root)
+        parent_basename = str(Path(root).parents[0]) if root != folder else os.path.basename(root)
+        parent_element = element_roots[parent_basename]
+        cur_element = None
+        # check for init files first
+        has_init = False
+        for filename in files:
+            if (filename.startswith("init")):
+                cur_element = new_instance(
+                    "ModuleScript" if root == folder else get_instance_type_from_extension(filename),
+                    parent_element)
+                give_script_properties(
+                    cur_element,
+                    "MainModule" if root == folder else dirname,
+                    "".join(open(root + "/" + filename, "r").readlines()))
+                element_roots[root] = cur_element
+                has_init = True
+                break
+        if (not has_init):
+            cur_element = new_instance("Folder", parent_element)
+            set_name(cur_element, dirname)
+            element_roots[root] = cur_element
+
+        # now iterate through the files
+
+        for filename in files:
+            if (filename.startswith("init")):
+                continue
+
+            element = new_instance(
+                "ModuleScript" if root == folder else get_instance_type_from_extension(filename),
+                cur_element)
+            give_script_properties(
+                element,
+                re.sub("(.client|.server)$", "", Path(filename).stem),
+                "".join(open(root + "/" + filename, "r").readlines()))
+
+
+
 
 
 
 root = new_plain_rbxmx()
-give_script_properties(new_instance("ModuleScript", root), "main", "aaaaaa")
+fs_to_tree(args.source, root)
 
 if (args.output):
-    writeOutput(args.output)
+    write_output(args.output)
